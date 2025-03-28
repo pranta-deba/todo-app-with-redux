058888aab1a01bf941d9b6fccb3447084200aeb9
Pranto Deb <rajprit0m@outlook.com>
2025-03-28 23:50:42 +0600

    [Install redux tolkit and react redux]

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
+import { Button } from "./components/ui/button";
+
 function App() {
   return (
     <>
       <h1 className="text-red-500">Hi</h1>
+      <Button className="cursor-pointer">My button</Button>
     </>
   );
 }
