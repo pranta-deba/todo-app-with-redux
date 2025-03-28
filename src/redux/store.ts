9461bafb5b01e57fcbb0bef20d6e665f55c98482
Pranto Deb <rajprit0m@outlook.com>
2025-03-29 00:01:23 +0600

    [Redux] : [configuration react redux : store, hook, features and todo slice]

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,5 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
+import todoReducer from "./features/todoSlice";
 
 export const store = configureStore({
-  reducer: {},
+  reducer: {
+    todos: todoReducer,
+  },
 });
+
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
