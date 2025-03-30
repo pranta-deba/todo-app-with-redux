8e9064fe3f6bb1c103417bca8b9e95e246764c4d
Pranto Deb <rajprit0m@outlook.com>
2025-03-30 18:22:22 +0600

    [UI] : add and filter

--- src/components/todo/TodoCard.tsx
+++ src/components/todo/TodoCard.tsx
@@ -1,13 +1,15 @@
+import { Button } from "../ui/button";
+
 const TodoCard = () => {
   return (
-    <div className="bg-purple-200 rounded-md w-full flex justify-between p-4">
+    <div className="rounded-md w-full flex justify-between p-4 border-b-2 shadow">
       <input type="checkbox" name="check" id="check" />
       <p>Todo Title</p>
       <p>Time</p>
       <p>Description</p>
       <div className="space-x-2">
-        <button>Delete</button>
-        <button>Edit</button>
+        <Button className="bg-red-500">Delete</Button>
+        <Button className="bg-[#5C53FE]">Edit</Button>
       </div>
     </div>
   );
--- src/components/todo/TodoContainer.tsx
+++ src/components/todo/TodoContainer.tsx
@@ -1,19 +1,21 @@
+import { Button } from "../ui/button";
 import TodoCard from "./TodoCard";
 
 const TodoContainer = () => {
   return (
     <div>
-      <div>
-        <button>Add todo</button>
+      <div className="flex justify-between mb-5">
+        <Button className="primary-bg text-xl font-semibold cursor-pointer">Add todo</Button>
         <button>filter</button>
       </div>
 
-      <div className="bg-purple-600 p-4 rounded-xl space-y-3">
-        
-        <TodoCard />
-        <TodoCard />
-        <TodoCard />
-        <TodoCard />
+      <div className="w-full h-full primary-bg p-1 rounded-xl">
+        <div className="w-full h-full bg-white rounded-xl space-y-2 p-3">
+          <TodoCard />
+          <TodoCard />
+          <TodoCard />
+          <TodoCard />
+        </div>
       </div>
     </div>
   );
